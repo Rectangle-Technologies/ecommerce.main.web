bcc4d190a39da3f3326dfd4d9f747db05752c220
MadMaxINDIAN <naman22khater@gmail.com>
2022-08-26 00:09:12 +0530

    added category layout and resolved merge conflicts

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,6 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import CategoriesLayout from "../components/CategoriesLayout";
 import DoubleTextComponent from "../components/DoubleText";
 import Caraousel from "../components/header/caraousel";
 import ProductLayout from "../components/ProductLayout";
@@ -37,6 +38,20 @@ const Landing = () => {
         </Grid>
       </Grid>
       <DoubleTextComponent backText="What We Have" frontText="Categories" />
+      <Grid container spacing={3} style={{ padding: "0px 7.5vw 0px 7.5vw", marginBottom: "50px" }}>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="/1.jpeg" title="Saree" />
+        </Grid>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="2.jpeg" title="Kurtis" />
+        </Grid>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="3.jpeg" title="Full Sets" />
+        </Grid>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="4.png" title="Jewellery" />
+        </Grid>
+      </Grid>
     </>
   );
 };
