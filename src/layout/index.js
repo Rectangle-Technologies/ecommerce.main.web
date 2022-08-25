3e451dea718cc5359eb62ee6263d698d820f3b91
MadMaxINDIAN <naman22khater@gmail.com>
2022-08-26 00:04:51 +0530

    Category layout added

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import React from "react";
 import { connect } from "react-redux";
+import CategoriesLayout from "../components/CategoriesLayout";
 import DoubleTextComponent from "../components/DoubleText";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header";
@@ -28,6 +29,20 @@ const Layout = (props) => {
         </Grid>
       </Grid>
       <DoubleTextComponent backText="What We Have" frontText="Categories" />
+      <Grid container spacing={3} style={{ padding: "0px 7.5vw 0px 7.5vw", marginBottom: "50px" }}>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="/1.jpeg" title="Saree" />
+        </Grid>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="2.jpeg" title="Kurtis" />
+        </Grid>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="3.jpeg" title="Full Sets" />
+        </Grid>
+        <Grid item xs={6} lg={3}>
+          <CategoriesLayout image="4.png" title="Jewellery" />
+        </Grid>
+      </Grid>
       <Footer />
     </>
   );
