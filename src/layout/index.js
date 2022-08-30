86e0ef0f350310e4a0213557a05589b3ddd4331c
MadMaxINDIAN <naman22khater@gmail.com>
2022-08-30 11:46:34 +0530

    social media fixed div added

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,6 @@
+import { Grid, Link } from "@mui/material";
 import React from "react";
+import { isMobile } from "react-device-detect";
 import { connect } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
@@ -10,6 +12,29 @@ const Layout = (props) => {
       <Header />
       <Outlet />
       <Footer />
+      <div style={{ position: "fixed", right: 0, top: "50%", width: isMobile ? "10vw" : "4vw", transform: "translate(0%, -50%)" }} >
+        <div style={{ position: "relative", top: "-50%" }}>
+          <Grid container>
+            <Link href="https://www.facebook.com" rel="noopener" target="_blank">
+            <Grid item xs={12}>
+              <img src="/fb.png" style={{ width: "100%" }} />
+            </Grid>
+            </Link>
+
+            <Grid item xs={12}>
+              <img src="/wa.png" style={{ width: "100%" }} />
+            </Grid>
+
+            <Grid item xs={12}>
+              <img src="/tw.png" style={{ width: "100%" }} />
+            </Grid>
+
+            <Grid item xs={12}>
+              <img src="/pn.png" style={{ width: "100%" }} />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
     </>
   );
 };
