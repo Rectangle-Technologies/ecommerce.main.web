a23f58e4302e0198db1febd15e98787788c1728e
Samyak Shah <samyakshah123@gmail.com>
2022-08-30 21:22:30 +0530

    Cart for desktop made

--- src/components/cart/desktop/ProductsDesktop.js
+++ src/components/cart/desktop/ProductsDesktop.js
@@ -1,8 +1,20 @@
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Link, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import textStyle from '../../../helpers/textStyle'
 
 const ProductsDesktop = () => {
+    const [quantity, setQuantity] = useState(1)
+
+    const updateQuantity = (action) => {
+        if (action === '+') {
+            setQuantity(quantity + 1);
+        } else {
+            if (quantity > 1) {
+                setQuantity(quantity - 1);
+            }
+        }
+    }
+
     return (
         <div style={{ display: 'flex' }}>
             <div style={{ display: 'flex', width: '50%', padding: 10 }}>
@@ -14,23 +26,70 @@ const ProductsDesktop = () => {
             <div style={{ width: '22%', margin: 'auto', padding: 10 }}>
                 <Typography style={{ ...textStyle, fontWeight: 500, fontSize: 24, textAlign: 'center' }} m={2}>Rs. 1150</Typography>
             </div>
-            <div style={{ width: '22%', margin: 'auto', padding: 10 }}>
-                <Typography style={{ ...textStyle, fontWeight: 500, fontSize: 20, textAlign: 'center' }} m={2}>1</Typography>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '22%', margin: 'auto', padding: 10 }}>
+                <Link style={{ cursor: 'pointer' }} onClick={() => updateQuantity('-')}>
+                    <div
+                        style={{
+                            border: "1px solid #222222",
+                            width: 40,
+                            height: 40,
+                            padding: "6px",
+                        }}
+                    >
+                        <Typography
+                            style={{
+                                ...textStyle,
+                                fontFamily: "Roboto",
+                                textAlign: "center",
+                            }}
+                        >
+                            -
+                        </Typography>
+                    </div>
+                </Link>
+                <div
+                    style={{
+                        border: "1px solid #222222",
+                        width: 40,
+                        height: 40,
+                        padding: "6px",
+                    }}
+                >
+                    <Typography
+                        style={{
+                            ...textStyle,
+                            fontFamily: "Roboto",
+                            textAlign: "center",
+                        }}
+                    >
+                        {quantity}
+                    </Typography>
+                </div>
+                <Link style={{ cursor: 'pointer' }} onClick={() => updateQuantity('+')}>
+                    <div
+                        style={{
+                            border: "1px solid #222222",
+                            width: 40,
+                            height: 40,
+                            padding: "6px",
+                        }}
+                    >
+                        <Typography
+                            style={{
+                                ...textStyle,
+                                fontFamily: "Roboto",
+                                textAlign: "center",
+                            }}
+                        >
+                            +
+                        </Typography>
+                    </div>
+                </Link>
             </div>
             <div style={{ width: '6%', margin: 'auto', padding: 10 }}>
                 <Typography style={{ ...textStyle, fontWeight: 600, fontSize: 20, textAlign: 'center' }} m={2}>X</Typography>
             </div>
         </div>
-        // <Grid container>
-        //     <Grid item xs={12} md={6}>
-        //         <div style={{ display: 'flex', flexDirection: 'row' }}>
-        //             <img src='https://i.pinimg.com/736x/33/66/50/336650d646d0f5d9e144e626323a3d42.jpg'
-        //                 style={{ width: '20%', aspectRatio: 0.65 }}
-        //             />
-        //             <Typography style={{ ...textStyle, fontWeight: 500 }} m={2}>Product Name</Typography>
-        //         </div>
-        //     </Grid>
-        // </Grid>
     )
 }
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,20 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import textStyle from '../helpers/textStyle'
 import ProductsDesktop from '../components/cart/desktop/ProductsDesktop'
 import { BrowserView } from 'react-device-detect'
+import { styled } from "@mui/material/styles";
 
 const Cart = () => {
+    const [instructions, setInstructions] = useState()
+    const [voucher, setVoucher] = useState()
+    const CustomButton = styled(Button)({
+        textTransform: "none",
+        backgroundColor: "#eb31e2",
+        "&:hover": {
+            backgroundColor: "#fc03cf",
+        },
+    });
     return (
         <>
             <BrowserView>
@@ -18,6 +28,69 @@ const Cart = () => {
                             <Typography style={{ ...textStyle, fontWeight: 600, fontSize: 20, width: '6%', textAlign: 'center' }}></Typography>
                         </div>
                         <ProductsDesktop />
+                        <ProductsDesktop />
+                        <ProductsDesktop />
+                    </div>
+                    <div style={{ marginTop: 10, width: '80%', margin: 'auto' }}>
+                        <Grid container my={3} spacing={5}>
+                            <Grid item xs={6}>
+                                <Typography style={{ ...textStyle, fontWeight: 600, fontSize: 20 }} my={2}>Additional Instructions</Typography>
+                                <TextField
+                                    variant='outlined'
+                                    fullWidth
+                                    placeholder='Instructions for the seller'
+                                    multiline
+                                    rows={4}
+                                    label='Instructions'
+                                    value={instructions}
+                                    onChange={(e) => setInstructions(e.target.value)}
+                                />
+                                <Typography style={{ ...textStyle, fontWeight: 600, fontSize: 20 }} my={2}>Apply Voucher</Typography>
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <TextField
+                                        variant='outlined'
+                                        placeholder='Voucher Code'
+                                        multiline
+                                        label='Voucher'
+                                        value={voucher}
+                                        onChange={(e) => setVoucher(e.target.value)}
+                                    />
+                                    <CustomButton variant="contained" size='small' sx={{ mx: 1 }}>
+                                        Apply
+                                    </CustomButton>
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography style={{ ...textStyle, fontWeight: 600, fontSize: 20 }} my={2}>ORDER SUMMARY</Typography>
+                                <div style={{ backgroundColor: '#BEB0B0 10%', padding: 15 }}>
+                                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                        <Typography style={{ ...textStyle, fontWeight: 500 }}>SUB TOTAL</Typography>
+                                        <Typography style={{ ...textStyle, fontWeight: 500 }}>Rs. 1500</Typography>
+                                    </div>
+                                    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 15 }}>
+                                        <Typography style={{ ...textStyle, fontWeight: 500 }}>DISCOUNT (if any)</Typography>
+                                        <Typography style={{ ...textStyle, fontWeight: 500 }}>-Rs. 100</Typography>
+                                    </div>
+                                    <hr style={{ backgroundColor: '#000000' }} />
+                                    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 15 }}>
+                                        <Typography style={{ ...textStyle, fontWeight: 500 }}>TOTAL AMOUNT</Typography>
+                                        <Typography style={{ ...textStyle, fontWeight: 500 }}>Rs. 1400</Typography>
+                                    </div>
+                                </div>
+                                <center>
+                                    <Link style={{ cursor: 'pointer' }}>
+                                        <div style={{ backgroundColor: '#FA861B', border: '1px solid #330C3E', width: '80%', padding: 10, marginTop: 15 }}>
+                                            <Typography style={{ ...textStyle, fontWeight: 500, color: '#F8F5CC', textAlign: 'center' }}>Proceed to Checkout</Typography>
+                                        </div>
+                                    </Link>
+                                    <Link style={{ cursor: 'pointer' }}>
+                                        <div style={{ backgroundColor: '#F8F5CC', border: '1px solid #330C3E', width: '80%', padding: 10, marginTop: 15 }}>
+                                            <Typography style={{ ...textStyle, fontWeight: 500, color: '#330C3E', textAlign: 'center' }}>Continue Shopping</Typography>
+                                        </div>
+                                    </Link>
+                                </center>
+                            </Grid>
+                        </Grid>
                     </div>
                 </div>
             </BrowserView>
