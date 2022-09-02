ab29dc58e69ef48c297f209ea34136a43de7b63b
MadMaxINDIAN <naman22khater@gmail.com>
2022-09-03 02:53:54 +0530

    Client diaries form added

--- src/components/ClientDiaries.js
+++ src/components/ClientDiaries.js
@@ -1,9 +1,69 @@
+import { Grid, Paper, Rating, Typography } from "@mui/material";
 import React from "react";
+import ClientDiariesForm from "./ClientDiariesForm";
+import DoubleTextComponent from "./DoubleText";
 
 const ClientDiariesComponent = (props) => {
     return (
         <>
-        Client diaries
+            <DoubleTextComponent frontText="Client Diaries" backText="What our Client says" />
+            <Grid container spacing={3} style={{ padding: "0px 8vw", marginBottom: "4em" }}>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r1.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r2.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r3.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r1.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r2.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r3.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r1.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r2.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <img style={{ width: "100%", aspectRatio: 1.75, objectFit: "cover", objectPosition: "top" }} src="/r3.jpeg" />
+                    <Typography style={{ fontFamily: "EB Garamond", fontSize: "16px", fontWeight: "700" }}>“ With thousands of stunning pieces and pleased clientele over the years “</Typography>
+                    <Rating value={4.75} precision={0.25} />
+                    <Typography style={{ fontFamily: "Poppins", fontStyle: "italic", color: "#330C3E", fontSize: "16px", fontWeight: "700" }}>- Neha Agrawal</Typography>
+                </Grid>
+            </Grid>
+            <ClientDiariesForm />
         </>
     )
 }
