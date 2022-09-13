03c9c9a7e5deb5aa80fb8424421138357d88593f
Samyak Shah <samyakshah123@gmail.com>
2022-09-13 22:00:24 +0530

    Added pagination to orders

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Link, Typography } from '@mui/material'
+import { Box, Grid, Link, Pagination, Typography } from '@mui/material'
 import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
@@ -14,6 +14,9 @@ import capitalizeFirstLetter from '../helpers/capitaliseFirst'
 
 const Orders = (props) => {
     const [orders, setOrders] = useState()
+    const [page, setPage] = useState(1)
+    const [maxPages, setMaxPages] = useState(1)
+    const limit = 10
     const navigate = useNavigate()
     const config = {
         headers: {
@@ -25,8 +28,10 @@ const Orders = (props) => {
     const fetchOrders = async () => {
         props.addLoader()
         try {
-            const res = await axios.get(`${BASE_URL_1}/order/fetch`, config)
+            const res = await axios.get(`${BASE_URL_1}/order/fetch?page=${page}&limit=${limit}`, config)
             setOrders(res.data.orders)
+            console.log(res.data)
+            setMaxPages(Math.ceil(res.data.count / limit))
             props.removeLoader()
         } catch (err) {
             props.removeLoader()
@@ -43,13 +48,17 @@ const Orders = (props) => {
         }
     }
 
+    const handlePageChange = (event, value) => {
+        setPage(value)
+    }
+
     useEffect(() => {
         if (!props?.auth?.isAuthenticated) {
             navigate('/login', { state: { navigateUrl: '/wishlist' } })
             return
         }
         fetchOrders()
-    }, [])
+    }, [page])
 
     return (
         <>
@@ -57,29 +66,34 @@ const Orders = (props) => {
             <div style={{ width: window.innerWidth > 600 ? '50%' : '100%', margin: 'auto', padding: '10px' }}>
                 {orders &&
                     orders?.length > 0
-                    ? orders?.map((order, idx) => (
-                        <Link style={{ cursor: 'pointer' }} onClick={() => navigate(`/order/${order?._id}`)}>
-                            <div style={{ padding: '10px', border: '1px solid black', margin: '20px 0px' }}>
-                                <Typography style={textStyle} component='div'>Status: <Box display='inline' color='#eb31e2'>{capitalizeFirstLetter(order?.status)}</Box> </Typography>
-                                <Grid key={idx} container my={1}>
-                                    <Grid item xs={3} mr={1}>
-                                        <img src={order?.products[0]?.productId?.imageUrls[0]} style={{ width: window.innerWidth > 600 ? '80%' : '100%', aspectRatio: 0.7 }} />
+                    ? <div>
+                        {orders?.map((order, idx) => (
+                            <Link style={{ cursor: 'pointer' }} onClick={() => navigate(`/order/${order?._id}`)}>
+                                <div style={{ padding: '10px', border: '1px solid black', margin: '20px 0px' }}>
+                                    <Typography style={textStyle} component='div'>Status: <Box display='inline' color='#eb31e2'>{capitalizeFirstLetter(order?.status)}</Box> </Typography>
+                                    <Grid key={idx} container my={1}>
+                                        <Grid item xs={3} mr={1}>
+                                            <img src={order?.products[0]?.productId?.imageUrls[0]} style={{ width: window.innerWidth > 600 ? '80%' : '100%', aspectRatio: 0.7 }} />
+                                        </Grid>
+                                        <Grid item xs={8}>
+                                            <Typography style={{ ...textStyle, textAlign: 'right' }}>
+                                                {new Date(order?.createdAt).toLocaleString('en-IN', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' })}
+                                            </Typography>
+                                            <Typography style={{ ...textStyle, fontSize: window.innerWidth > 600 ? 20 : 16 }} my={1}>
+                                                {order?.products[0]?.productId?.name}
+                                            </Typography>
+                                            <Typography style={{ ...textStyle, fontSize: window.innerWidth > 600 ? 20 : 16 }} my={1}>
+                                                Total: {formatAmount(order?.amount)}
+                                            </Typography>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item xs={8}>
-                                        <Typography style={{ ...textStyle, textAlign: 'right' }}>
-                                            {new Date(order?.createdAt).toLocaleString('en-IN', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' })}
-                                        </Typography>
-                                        <Typography style={{ ...textStyle, fontSize: window.innerWidth > 600 ? 20 : 16 }} my={1}>
-                                            {order?.products[0]?.productId?.name}
-                                        </Typography>
-                                        <Typography style={{ ...textStyle, fontSize: window.innerWidth > 600 ? 20 : 16 }} my={1}>
-                                            Total: {formatAmount(order?.amount)}
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                            </div>
-                        </Link>
-                    ))
+                                </div>
+                            </Link>
+                        ))}
+                        <div style={{ display: 'flex', justifyContent: 'center', margin: '15px 0px' }}>
+                            <Pagination count={maxPages} page={page} onChange={handlePageChange} />
+                        </div>
+                    </div>
                     : <Typography stye={{ ...textStyle, fontSize: 24, textAlign: 'center' }}>You have no orders</Typography>
                 }
             </div>
