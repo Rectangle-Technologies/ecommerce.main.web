981b80304ad68dbd175ca9a8d1fca76bd7aad08b
Samyak Shah <samyakshah123@gmail.com>
2022-09-23 12:39:55 +0530

    Changed amount format

--- src/components/ProductLayout.js
+++ src/components/ProductLayout.js
@@ -3,6 +3,7 @@ import AddIcon from "@mui/icons-material/AddCircleTwoTone";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import React from "react";
+import formatAmount from "../helpers/formatAmount";
 
 const ProductLayout = (props) => {
     const newButton = props.new ? { backgroundColor: "white", color: "#EB31E2" } : {};
@@ -11,8 +12,8 @@ const ProductLayout = (props) => {
         <>
             <div style={{ position: "relative" }}>
                 <img style={{ aspectRatio: 0.65, width: "100%", objectFit: "cover", borderRadius: "5px" }} src={props.imageUrl} />
-                
-                
+
+
                 <div style={{ marginTop: "10px" }}>
                     <u>
                         <Typography style={{ fontSize: 14 }}>
@@ -20,7 +21,7 @@ const ProductLayout = (props) => {
                         </Typography>
                     </u>
                     <Typography style={{ fontSize: 14, fontWeight: "bold" }}>
-                        Rs. {props.mrp}
+                        {formatAmount(props.mrp)}
                     </Typography>
                 </div>
             </div>
