e63a79d5f3a9af51473e67eea7c0493a56decdb5
Samyak Shah <samyakshah123@gmail.com>
2022-09-30 17:42:31 +0530

    Fixed authentication bug

--- src/redux/services/actions/exchangeActions.js
+++ src/redux/services/actions/exchangeActions.js
@@ -1,20 +1,21 @@
 import { post } from "../../../utils/apiHelper"
 
 export const addNewQuery = (data, token, enqueueSnackbar, setSubmitting, resetForm) => async (dispatch) => {
-    post("http://localhost:5000/exchange/query/new", token, data)
-    .then((res) => {
-        enqueueSnackbar(res.data.message, {
-            autoHideDuration: 3000,
-            variant: "success"
+    post("http://localhost:5000/exchange/query/new", `Bearer ${token}`, data)
+        .then((res) => {
+            enqueueSnackbar(res.data.message, {
+                autoHideDuration: 3000,
+                variant: "success"
+            })
+            setSubmitting(false);
+            resetForm();
         })
-        setSubmitting(false);
-        resetForm();
-    })
-    .catch((err) => {
-        console.log(err)
-        enqueueSnackbar(err.response.data.message, {
-            autoHideDuration: 3000,
-            variant: "error"
+        .catch((err) => {
+            console.log(err)
+            setSubmitting(false);
+            enqueueSnackbar(err.response.data.message, {
+                autoHideDuration: 3000,
+                variant: "error"
+            })
         })
-    })
 }
