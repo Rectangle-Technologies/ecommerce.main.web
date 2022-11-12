20fdb85e070c37a3e2841e8e20dfaf88b05a2136
Samyak Shah <samyakshah123@gmail.com>
2022-11-12 20:13:42 +0530

    Created build

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
-export const BASE_URL_1 = 'http://localhost:4000'
-export const BASE_URL_2 = 'https://bloom-product.herokuapp.com'
-export const BASE_URL_3 = 'https://bloom-payment.herokuapp.com'
-export const WEBSOCKET_URL = "ws://bloom-user.herokuapp.com/";
+export const BASE_URL_1 = 'http://ec2-65-1-95-63.ap-south-1.compute.amazonaws.com:4000'
+export const BASE_URL_2 = 'http://ec2-65-1-95-63.ap-south-1.compute.amazonaws.com:5000'
+export const BASE_URL_3 = 'http://ec2-65-1-95-63.ap-south-1.compute.amazonaws.com:7789'
+export const WEBSOCKET_URL = "ws://ec2-65-1-95-63.ap-south-1.compute.amazonaws.com:4000/";
