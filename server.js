67311ee7752ef7fd97269db1f56c3a9bb79e9a2c
MadMaxINDIAN <naman22khater@gmail.com>
2022-11-14 10:40:40 +0530

    server config updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static("build"));
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // start express server on port 5000
