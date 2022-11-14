15136b2ba2c2bf637c701a1d992d24bc24a96cdb
Samyak Shah <samyakshah123@gmail.com>
2022-11-14 12:10:42 +0530

    Build

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
 export const BASE_URL_1 = 'http://ec2-65-0-199-224.ap-south-1.compute.amazonaws.com:4000'
-export const BASE_URL_2 = 'http://ec2-65-0-199-224.ap-south-1.compute.amazonaws.com:5000'
+export const BASE_URL_2 = 'http://ec2-3-109-3-14.ap-south-1.compute.amazonaws.com:5000'
 export const BASE_URL_3 = 'http://ec2-65-0-199-224.ap-south-1.compute.amazonaws.com:7789'
 export const WEBSOCKET_URL = "ws://ec2-65-0-199-224.ap-south-1.compute.amazonaws.com:4000/";
