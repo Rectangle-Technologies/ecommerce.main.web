63819b336afd69b715d60cd6fc4903d66161bb9c
Samyak Shah <samyakshah123@gmail.com>
2023-02-11 00:19:30 +0530

    Build

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -5,7 +5,7 @@
 // export const WEBSOCKET_URL = "ws://localhost:4000/";
 
 // Deployed
-export const BASE_URL_1 = 'http://ec2-3-109-55-89.ap-south-1.compute.amazonaws.com:4000'
-export const BASE_URL_2 = 'http://ec2-3-108-220-134.ap-south-1.compute.amazonaws.com:5000'
-export const BASE_URL_3 = 'http://ec2-3-109-55-89.ap-south-1.compute.amazonaws.com:7789'
-export const WEBSOCKET_URL = "ws://ec2-3-109-55-89.ap-south-1.compute.amazonaws.com:4000/";
+export const BASE_URL_1 = 'https://user-backend.bloombykhushbu.in'
+export const BASE_URL_2 = 'https://product-backend.bloombykhushbu.in'
+export const BASE_URL_3 = 'https://payment-backend.bloombykhushbu.in'
+export const WEBSOCKET_URL = "wss://user-backend.bloombykhushbu.in/";
